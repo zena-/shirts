5b5e6a4a8b5d40a5849d52028103e2742377dfa4
zena <e.nosazena@gmail.com>
2014-05-26 17:11:04 -0400

    updated products and shirt.php

--- inc/products.php
+++ inc/products.php
@@ -334,4 +334,33 @@ function get_products_all() {
         return $products;
 }
 
+/*returns an array of products info for the product that matches the sku;
+*returns a boolean false if no products matches the sku
+*@param     int     $sku    the sku
+*@return    mixed   array   list of product info for the one matching product
+*                   bool    false if no product matches
+*/
+
+function get_product_single($sku) {
+    require(ROOT_PATH . "inc/database.php");
+
+    try {
+        $results = $db->prepare("SELECT name, price, img, sku, paypal FROM products WHERE sku = ?");
+        $results->bindParam(1,$sku);
+        $results->execute();
+    } catch (Exception $e) {
+        echo "Data could not be retrieved from the database";
+        exit
+    }
+
+    $product = $results->fetch(PDO::FETCH_ASSOC);
+
+    // echo "<pre>"
+    // var_dump($product);
+    // exit
+
+    return $product;
+}
+
+
 ?>
--- shirts/shirt.php
+++ shirts/shirt.php
@@ -2,24 +2,25 @@
 
 	require_once("../inc/config.php");
 	require_once(ROOT_PATH . "inc/products.php");
-	$products = get_products_all();
+	// $products = get_products_all();
 
 // if an ID is specified in the query string, use it
 if (isset($_GET["id"])) {
-	$product_id = $_GET["id"];
+	$product_id = intval($_GET["id"]);
+	$product = get_product_single($product_id)
 
 	// if there is a real product that corresponds to the ID
 	// specified in the query string, use that for $product
-	if (isset($products[$product_id])) {
-		$product = $products[$product_id];
-	}
+	// if (isset($products[$product_id])) {
+	// 	$product = $products[$product_id];
+	// }
 }
 
-// a $product will only be set if an ID is specified in the query
+// a $product will only be set and not false if an ID is specified in the query
 // string and it corresponds to a real product. If no product is
 // set, then redirect to the shirts listing page; otherwise, continue
 // on and display the Shirt Details page for that $product
-if (!isset($product)) {
+if (empty($product)) {
 	header("Location: " . BASE_URL . "shirts/");
 	exit();
 }
